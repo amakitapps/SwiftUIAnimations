9f2d2090bfd4f3d8636b5ab207fc16376835d308
Mark Nair <mark.nair@actx.edu>
2020-02-06 23:21:38 -0600

    Added heart code

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var circleColorChanged = false
+    @State private var heartColorChanged = false
+    @State private var heartSizeChanged = false
+
     var body: some View {
-        Text("Hello World")
+        
+        ZStack {
+            Circle()
+            .frame(width: 200, height: 200)
+                .foregroundColor(circleColorChanged ? .gray : .red)
+            
+            Image(systemName: "heart.fill")
+                .foregroundColor(heartColorChanged ? .red : .white)
+                .font(.system(size: 100))
+                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+        }
+        .onTapGesture {
+            self.circleColorChanged.toggle()
+            self.heartColorChanged.toggle()
+            self.heartSizeChanged.toggle()
+        }
     }
 }
 
